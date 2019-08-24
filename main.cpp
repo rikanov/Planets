7aa6e9db325200f380f3e6d7907ecbec6bbf831c
Robert Ikanov <robert.ikanov@gmail.com>
2019-08-24 19:15:52 +0200

    [m] basic ui test

--- main.cpp
+++ main.cpp
@@ -1,33 +1,10 @@
 #include "global.h"
-#include "board.h"
+#include "basicui.h"
 #include "generator.h"
-//#include <Windows.h>
 
 int main()
 {
-    Board teszt;
-    teszt.reset();
-    for (int turn = 0; turn < 2; ++turn)
-    {
-        Generator T(turn == 0 ? teszt._collectionOfPlayer : teszt._collectionOfProgram);
-        Step next;
-        T.randomize();
-        T.reset();
-        while (T.nextRandom(next))
-        {
-            next.step();
-            CLR();
-            teszt.show();
-            Sleep(1);
-
-            next.back();
-            /* CLR();
-             teszt.show();
-             Sleep(500);*/
-        }
-    }
-    log("finished")
-    char c;
-    std::cin >> c;
+    BasicUI ui;
+    ui.start();
     return 0;
 }
